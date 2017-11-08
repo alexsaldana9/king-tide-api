04640d4c52ea680daf3e2b61dd1938a646bb5dfb
Alexandra Saldana <alexsaldana9@gmail.com>
2017-11-07 21:01:53 -0500

    authorization verification method added to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::API
+
+  def request_is_authorized
+
+    # Secretkey.where(key: apiKey).empty?
+    # this looks for the secret key that is passed as a request.header
+    # if it is false, it means that there is a valid apiKey that matches the header
+
+    apiKey = request.headers["apiKey"]
+    if Secretkey.where(key: apiKey).empty? == false
+      return true
+    else
+      return false
+    end
+  end
+
 end
--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -11,53 +11,83 @@ class ReadingsController < ApplicationController
   end
 
   def create
-    apiKey = request.headers["apiKey"]
-    puts "value of api key #{apiKey}"
+    # apiKey = request.headers["apiKey"]
+    # puts "value of api key #{apiKey}"
 
-    if Secretkey.where(key: apiKey).empty?
-      @error = 'Error saving reading'
-      render :json => @error, status: 401
-    else
+    #TODO: move this to the ApplicationController
+    #TODO: secure all the other api methods
+
+    if request_is_authorized == true
       @reading = Reading.new(reading_params)
       @reading.save
       render :json => @reading
+    else
+      render :json => {error: 'Unauthorized'}, status: 401
     end
+
+
+    # if Secretkey.where(key: apiKey).empty?
+    #   @error = 'Error saving reading'
+    #   render :json => @error, status: 401
+    # else
+    #   @reading = Reading.new(reading_params)
+    #   @reading.save
+    #   render :json => @reading
+    # end
   end
 
   def update
     # Need to allow this method only for admin, add another field to get the name of person that edited
-    @reading = Reading.find_by(id: params[:id])
-    if @reading.update(reading_params)
-      render :json => @reading
-    else
+    apiKey = request.headers["apiKey"]
+    if Secretkey.where(key: apiKey).empty?
       @error = 'Error updating the reading'
-      render :json => @error, status: 400
+      render :json => @error.to_json, status: 400
+    else
+      @reading = Reading.find_by(id: params[:id])
+      @reading.update(reading_params)
+      render :json => @reading.to_json
     end
+
+
+    # @reading = Reading.find_by(id: params[:id])
+    # if @reading.update(reading_params)
+    #   render :json => @reading
+    # else
+    #   @error = 'Error updating the reading'
+    #   render :json => @error, status: 400
+    # end
   end
 
   def delete
     # Need to allow this method only for admin, add another field to get the name of person that deleted
     #Or maybe this method is not necessary, this can complicate the data management
-    if @reading = Reading.find_by(id: params[:id])
-      @reading.delete
-      @message = 'Record deleted'
-      render :json => @message
-    else
+
+    apiKey = request.headers["apiKey"]
+    if Secretkey.where(key: apiKey).empty?
       @error = 'Error deleting the reading'
-      render :json => @error
+      render :json => @error.to_json
+    else
+      @reading = Reading.find_by(id: params[:id])
+      @reading.destroy
+      @message = 'Record deleted'
+      render :json => @message.to_json
     end
 
 
-  end
+    # if @reading = Reading.find_by(id: params[:id])
+    #   @reading.delete
+    #   @message = 'Record deleted'
+    #   render :json => @message
+    # else
+    #   @error = 'Error deleting the reading'
+    #   render :json => @error
+    # end
 
-  def status
-    @message = 'Status OK'
-    render :json => @message
+
   end
 
   def reading_params
     params.permit(:depth, :units_depth, :salinity, :units_salinity, :description)
   end
-
 
 end
