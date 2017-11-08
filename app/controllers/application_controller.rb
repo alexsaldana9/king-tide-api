2689df384217626be97c3587922d7b7582f53adf
Alexandra Saldana <alexsaldana9@gmail.com>
2017-11-07 21:29:39 -0500

    Added request_is_not_authorized method to each POST method, to remove nesting

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,10 @@
 class ApplicationController < ActionController::API
 
-  def request_is_authorized
+  def request_is_not_authorized
+    return request_is_authorized == false
+  end
 
+  def request_is_authorized
     # Secretkey.where(key: apiKey).empty?
     # this looks for the secret key that is passed as a request.header
     # if it is false, it means that there is a valid apiKey that matches the header
--- app/controllers/readings_controller.rb
+++ app/controllers/readings_controller.rb
@@ -12,42 +12,38 @@ class ReadingsController < ApplicationController
 
   def create
     #TODO: move this to the ApplicationController
-    #TODO: secure all the other api methods
     #TODO: remove the users controller/model/etc
 
-    if request_is_authorized == true
-      reading = Reading.new(reading_params)
-      reading.save
-      render :json => reading
-    else
-      unauthorized_request
+    if request_is_not_authorized
+      return unauthorized_request
     end
 
+    reading = Reading.new(reading_params)
+    reading.save
+    render :json => reading
   end
 
   def update
-
-    if request_is_authorized == true
-      reading = Reading.find_by(id: params[:id])
-      reading.update(reading_params)
-      render :json => reading
-    else
-      unauthorized_request
+    if request_is_not_authorized
+      return unauthorized_request
     end
 
+    reading = Reading.find_by(id: params[:id])
+    reading.update(reading_params)
+    render :json => reading
   end
 
   def delete
     # Need to allow this method only for admin, add another field to get the name of person that deleted
     #Or maybe this method is not necessary, this can complicate the data management
 
-    if request_is_authorized == true
-      reading = Reading.find_by(id: params[:id])
-      reading.destroy
-      render :json => {status: 'Record deleted'}
-    else
-      unauthorized_request
+    if request_is_not_authorized
+      return unauthorized_request
     end
+
+    reading = Reading.find_by(id: params[:id])
+    reading.destroy
+    render :json => {status: 'Record deleted'}
 
   end
 
