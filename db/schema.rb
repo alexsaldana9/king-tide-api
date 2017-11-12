dc6a9971448dc20d6d5dbc264375787a0ba4824f
Alexandra Saldana <alexsaldana9@gmail.com>
2017-11-12 10:03:24 -0500

    Run the tests against sqlite, which is way faster

--- db/schema.rb
+++ db/schema.rb
@@ -12,9 +12,6 @@
 
 ActiveRecord::Schema.define(version: 20171111222511) do
 
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
   create_table "readings", force: :cascade do |t|
     t.float "depth"
     t.string "units_depth"
