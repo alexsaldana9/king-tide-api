76e3c987dc65c9da5f33aa501bb90361a0844378
Alexandra Saldana <alexsaldana9@gmail.com>
2018-01-10 22:11:50 -0500

    Secure the photo creation endpoint

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,19 +1,19 @@
 class PhotosController < ApplicationController
 
   def create
-    # @photo = Photo.new(photo_params)
-    #
-    # if @photo.save
-    #   redirect_to @photo, notice: 'Photo was successfully uploaded.'
-    # else
-    #   render action: 'new'
-    # end
+    if request_is_not_authorized
+      return unauthorized_request
+    end
+
+
 
     photo = Photo.create(photo_params)
+    if not photo.valid?
+      return render :json => {error: 'Photo not saved'}, status: 400
+    end
 
-    render :json => photo
+    render :json => {result: :ok}
   end
-
 
   private
 
