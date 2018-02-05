30cbb1e8b1ca57c2cc3a10c1ca7aeaccfe2920ba
Camilo Sanchez <camilin87@gmail.com>
2018-02-05 18:59:11 -0500

    correct typo in the default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root to: 'readings#getall'
+  root to: 'readings#get_all'
 
   get 'status/get', to: 'status#get_status'
 
