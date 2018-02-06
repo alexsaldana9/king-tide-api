a5aeec10020b4e2441e1c4ec432054f50e4c7f08
Camilo Sanchez <camilin87@gmail.com>
2018-02-06 12:39:36 -0500

    no need for rspec anymore

--- spec/support/api_helper.rb
+++
@@ -1,7 +0,0 @@
-module ApiHelper
-  include Rack::Test::Methods
-
-  def app
-    Rails.application
-  end
-end
--- spec/support/request_helper.rb
+++
@@ -1,7 +0,0 @@
-module Requests
-  module JsonHelpers
-    def json
-      JSON.parse(last_response.body)
-    end
-  end
-end
