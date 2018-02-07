5c685b162308300bcd6e60b3823f2b42ffb0afb4
Camilo Sanchez <camilin87@gmail.com>
2018-02-06 22:36:37 -0500

    enhance the photos controller test setups

--- test/controllers/secure/photos_controller_test.rb
+++ test/controllers/secure/photos_controller_test.rb
@@ -6,21 +6,32 @@ class Secure::PhotosControllerTest < ActionDispatch::IntegrationTest
   end
 
   test 'when key is not passed, photo is not created' do
-    post '/photos', params: { reading_id: @r1.id }
+    post '/photos', params: {
+        reading_id: @r1.id,
+        category: 1,
+        image: test_image()
+    }
 
     assert_response 401
     assert_equal true, Photo.all.empty?
   end
 
   test 'when key is invalid, photo not created' do
-    post '/photos', params: { reading_id: @r1.id }, headers: {'apiKey' => 'invalidKEY'}
+    post '/photos', params: {
+        reading_id: @r1.id,
+        category: 1,
+        image: test_image()
+    }, headers: {'apiKey' => 'invalidKEY'}
 
     assert_response 401
     assert_equal true, Photo.all.empty?
   end
 
   test 'does not create photo when category is missing' do
-    post '/photos', params: { reading_id: @r1.id }, headers: {'apiKey' => 'keysample'}
+    post '/photos', params: {
+        reading_id: @r1.id,
+        image: test_image()
+    }, headers: {'apiKey' => 'keysample'}
 
     assert_response 400
     assert_equal true, Photo.all.empty?
@@ -29,7 +40,8 @@ class Secure::PhotosControllerTest < ActionDispatch::IntegrationTest
   test 'does not create photo when category is invalid' do
     post '/photos', params: {
         reading_id: @r1.id,
-        category: 'invalid'
+        category: 'invalid',
+        image: test_image()
     }, headers: {
         'apiKey' => 'keysample'
     }
@@ -42,7 +54,8 @@ class Secure::PhotosControllerTest < ActionDispatch::IntegrationTest
     [-1, 0, 5, 6].each do |category|
       post '/photos', params: {
           reading_id: @r1.id,
-          category: category
+          category: category,
+          image: test_image()
       }, headers: {
           'apiKey' => 'keysample'
       }
@@ -54,7 +67,8 @@ class Secure::PhotosControllerTest < ActionDispatch::IntegrationTest
 
   test 'does not create photo when reading_id is missing' do
     post '/photos', params: {
-        category: 1
+        category: 1,
+        image: test_image()
     }, headers: {
         'apiKey' => 'keysample'
     }
@@ -66,7 +80,8 @@ class Secure::PhotosControllerTest < ActionDispatch::IntegrationTest
   test 'does not create photo when reading_id is invalid' do
     post '/photos', params: {
         reading_id: 'invalid',
-        category: 1
+        category: 1,
+        image: test_image()
     }, headers: {
         'apiKey' => 'keysample'
     }
@@ -78,7 +93,8 @@ class Secure::PhotosControllerTest < ActionDispatch::IntegrationTest
   test 'does not create photo when reading_id is not in the db' do
     post '/photos', params: {
         reading_id: -1,
-        category: 1
+        category: 1,
+        image: test_image()
     }, headers: {
         'apiKey' => 'keysample'
     }
@@ -103,7 +119,7 @@ class Secure::PhotosControllerTest < ActionDispatch::IntegrationTest
     post '/photos', params: {
         reading_id: @r1.id,
         category: 1,
-        image: Rack::Test::UploadedFile.new('test/fixtures/files/not_a_photo.txt')
+        image: test_image('not_a_photo.txt', 'text/plain')
     }, headers: {
         'apiKey' => 'keysample'
     }
@@ -118,7 +134,7 @@ class Secure::PhotosControllerTest < ActionDispatch::IntegrationTest
       post '/photos', params: {
           reading_id: @r1.id,
           category: 1,
-          image: Rack::Test::UploadedFile.new("test/fixtures/files/#{img}", 'image/jpeg')
+          image: test_image(img)
       }, headers: {
           'apiKey' => 'keysample'
       }
@@ -136,5 +152,11 @@ class Secure::PhotosControllerTest < ActionDispatch::IntegrationTest
     seeded_filenames.each do |f|
       assert photo_urls.any? {|u| u.include? f }
     end
+  end
+
+  private
+
+  def test_image(filename='test_image_1.jpg', content_type='image/jpeg')
+    return Rack::Test::UploadedFile.new("test/fixtures/files/#{filename}", content_type)
   end
 end
