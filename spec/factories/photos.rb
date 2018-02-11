92a19d0acad871c6c0396dd8f2ab4f2b20642fd4
Camilo Sanchez <camilin87@gmail.com>
2018-02-10 22:56:03 -0500

    annotate missing factory

--- spec/factories/photos.rb
+++ spec/factories/photos.rb
@@ -1,3 +1,28 @@
+# == Schema Information
+#
+# Table name: photos
+#
+#  id                 :integer          not null, primary key
+#  reading_id         :integer
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  image_file_name    :string
+#  image_content_type :string
+#  image_file_size    :integer
+#  image_updated_at   :datetime
+#  category           :integer
+#  deleted_at         :datetime
+#
+# Indexes
+#
+#  index_photos_on_deleted_at  (deleted_at)
+#  index_photos_on_reading_id  (reading_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (reading_id => readings.id)
+#
+
 FactoryBot.define do
   factory :photo do
     sequence :category do |n|
